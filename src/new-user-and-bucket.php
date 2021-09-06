3b7bb9be1ead611640ac9e1cc348fe6c8e2d0dde
Grigori Kochanov <grigori@grik.net>
2021-09-06 23:39:27 +0300

    Create, configure an sqs queue for a bucket

--- src/new-user-and-bucket.php
+++ src/new-user-and-bucket.php
@@ -11,9 +11,15 @@ use Aws\Iam\IamClient, Aws\Exception\AwsException;
 
 // AWS configuration values
 $AWS_API_CONSUMERS_GROUP = 'api-consumers';
-$AWS_BUCKET_NAME_PREFIX = 'gk-test-';
-const BUCKET_LIFECYCLE_DAYS = 62; // 2 months
+$AWS_BUCKET_NAME_PREFIX = 'acme-test-';
+$AWS_QUEUE_NAME_SUFFIX = '-queue';
 
+// delete objects older than
+const BUCKET_LIFECYCLE_DAYS = 62; // 2 months
+// drop messages in sqs after
+const SQS_MESSAGE_RETENTION_SECONDS = 172800; // 2 days
+// messages received but not deleted appear again after
+const SQS_VISIBILITY_TIMEOUT = 240; // 4 minutes
 
 // In a real application consumer details come from a message bus
 $customer_login = 'user@example.com';
@@ -80,14 +86,14 @@ try {
 
 
 // Create an AWS bucket
-$bucketName = $AWS_BUCKET_NAME_PREFIX . $customer_id;
-try {
-    $s3 = $sdk->createS3(['version'=> '2006-03-01']);
+$bucket_name = $AWS_BUCKET_NAME_PREFIX . $customer_id;
+$s3 = $sdk->createS3(['version'=> '2006-03-01']);
 
-    $s3->createBucket(['Bucket' => $bucketName]);
+try {
+    $s3->createBucket(['Bucket' => $bucket_name]);
 
     $s3->putPublicAccessBlock([
-        'Bucket' => $bucketName,
+        'Bucket' => $bucket_name,
         'PublicAccessBlockConfiguration' => [
             'BlockPublicAcls' => true,
             'BlockPublicPolicy' => true,
@@ -96,7 +102,7 @@ try {
         ],
     ]);
 
-    $policy = '{
+    $bucket_policy = '{
         "Version": "2012-10-17",
             "Statement": [
                 {
@@ -111,21 +117,21 @@ try {
                         "s3:GetObjectVersion"
                     ],
                     "Resource": [
-                        "arn:aws:s3:::'.$bucketName.'",
-                        "arn:aws:s3:::'.$bucketName.'/*"
+                        "arn:aws:s3:::'.$bucket_name.'",
+                        "arn:aws:s3:::'.$bucket_name.'/*"
                     ]
                 }
             ]
         }';
 
     $s3->putBucketPolicy([
-        'Bucket' => $bucketName,
-        'Policy' => $policy,
+        'Bucket' => $bucket_name,
+        'Policy' => $bucket_policy,
     ]);
 
     $result = $s3->putBucketLifecycle(
         [
-            'Bucket' => $bucketName,
+            'Bucket' => $bucket_name,
             'LifecycleConfiguration' => [
                 'Rules' => [
                     [
@@ -142,19 +148,103 @@ try {
     );
 
     $s3->putBucketEncryption([
-        'Bucket' => $bucketName,
+        'Bucket' => $bucket_name,
         'ServerSideEncryptionConfiguration' => [
             'Rules' => [['ApplyServerSideEncryptionByDefault' => ['SSEAlgorithm' => 'AES256']]]
         ]
     ]);
 
-    echo 'S3 bucket created: ',$bucketName,PHP_EOL;
+    $bucket_arn = 'arn:aws:s3:::'.$bucket_name;
+
+    echo 'S3 bucket created: ',$bucket_name,PHP_EOL;
 
 } catch (AwsException $e) {
     if ($e->getAwsErrorCode() != 'BucketAlreadyOwnedByYou') {
         error_log($e->getMessage());
         exit;
     }
-    echo 'S3 bucket exists: ',$bucketName,PHP_EOL;
+    echo 'S3 bucket exists: ',$bucket_name,PHP_EOL;
 }
 
+
+// Creaate an SQS queue
+$queue_name = $AWS_BUCKET_NAME_PREFIX . $customer_id . $AWS_QUEUE_NAME_SUFFIX;
+
+$sqs = $sdk->createSqs(['version' => '2012-11-05']);
+
+try {
+    $result = $sqs->createQueue(array(
+        'QueueName' => $queue_name,
+        'Attributes' => array(
+            'MessageRetentionPeriod' => SQS_MESSAGE_RETENTION_SECONDS,
+            'VisibilityTimeout' => SQS_VISIBILITY_TIMEOUT,
+        ),
+    ));
+    $queue_url = $result['QueueUrl'];
+    $result = $sqs->getQueueAttributes([
+        'QueueUrl' => $queue_url,
+        'AttributeNames' => ['QueueArn']
+    ]);
+    $queue_arn = $result['Attributes']['QueueArn'];
+} catch (AwsException $e) {
+    error_log($e->getMessage());
+    exit();
+}
+
+$queue_policy = '{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "__sender_statement",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Action": "SQS:SendMessage",
+      "Resource": "'.$queue_arn.'",
+      "Condition": {
+        "ArnLike": {
+          "aws:SourceArn": "arn:aws:s3:*:*:'.$bucket_name.'"
+        }
+      }
+    },
+    {
+      "Sid": "__receiver_statement",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "'.$consumer_arn.'"
+      },
+      "Action": [
+        "SQS:ChangeMessageVisibility",
+        "SQS:DeleteMessage",
+        "SQS:ReceiveMessage"
+      ],
+      "Resource": "'.$queue_name.'"
+    }
+  ]
+}';
+
+try {
+    $sqs->setQueueAttributes([
+        'QueueUrl' => $queue_url,
+        'Attributes'=>['Policy'=>$queue_policy]
+    ]);
+
+    $s3->putBucketNotificationConfiguration([
+    'Bucket' => $bucket_name,
+    'NotificationConfiguration' => [
+        'QueueConfigurations' => [
+            [
+                'Events' => ['s3:ObjectCreated:*'],
+                'Id' => $bucket_name.'-notification',
+                'QueueArn' => $queue_arn
+            ],
+        ],
+    ]]);
+
+} catch (AwsException $e) {
+    error_log($e->getMessage());
+    exit();
+}
+
+echo 'Queue configured: ',$queue_url,PHP_EOL;
